75e89c7d626c5583c5deaf05997b5c3e4fdd7679
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-10-24 18:42:56 +0900

    #1 make sharepreperance

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/sharedpreperance/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/sharedpreperance/MainActivity.java
@@ -1,13 +1,39 @@
 package kr.hs.emirim.dbwlsdud0407.sharedpreperance;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editName, editAge;
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName=(EditText)findViewById(R.id.edit_name);
+        editAge=(EditText)findViewById(R.id.edit_age);
+        sp=getSharedPreferences("appInfo", Context.MODE_PRIVATE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String name=sp.getString("name"," ");
+        String age=sp.getString("age"," ");
+        editName.setText(name);
+        editAge.setText(age);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences.Editor edit=sp.edit() ;
+        edit.putString("name",editName.getText().toString());
+        edit.putString("age",editAge.getText().toString());
+        edit.commit();
+
     }
 }
